2afa6aed70b0fff0e9700482f1f8637926dbf1ce
oobarbazanoo <yayarokya@mail.ru>
2017-02-23 16:59:03 +0200

    Done for int

--- js/main.js
+++ js/main.js
@@ -4,22 +4,31 @@
 
 $(function()
 {
+    var EMPTY_SET = "∅";
     $("#button").click(function()
     {
         var from1 = $("#from1").val(),
             to1 = $("#to1").val(),
             from2 = $("#from2").val(),
-            to2 = $("#to2").val(),
-            emptySet = "∅";
+            to2 = $("#to2").val();
 
-        if(boundariesNotСoincide(from1, to1, from2, to2))
-            {giveAnswer(emptySet);}
+        var from, to;
+        for(var i = from1; i <= to1; i++)
+        {
+            if(!from && belongs(i, from2, to2))
+                {from = i;}
+            else if(from && belongs(i, from2, to2))
+                {to = i;}
+        }
 
+        if(from)
+            {$("#showAnswer").text("Intersection: " + "[" + from + "," + to + "]");}
+        else
+            {$("#showAnswer").text("Intersection: " + EMPTY_SET);}
     });
 
 
-    function boundariesNotСoincide(from1, to1, from2, to2)
-    {
-        
-    }
+    function belongs(point, from, to)
+    {return (from <= point) && (point <= to);}
+
 })
